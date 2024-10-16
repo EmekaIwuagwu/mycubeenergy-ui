5aff16d233d745bf2fb30d032fc775711b0ed61b
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-10-16 20:28:36 +0100

    Admin's Pages completed

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, About, Login, Dashboard, Transactions, Bills, Settings, SignUp, ResetPassword, Shareunits, Topupwallet, Topupunits} from './modules/website/pages';
+import { Home, About, Login, Dashboard, Transactions, Bills, Settings, SignUp, ResetPassword, Shareunits,
+   Topupwallet, Topupunits, AdminLogin, AdminSignup, AdminDashboard, AdminOrder, AdminDashboardOrder,
+    AdminBilling, AdminUser, AdminManagement, AdminManagementDetails, AdminSettings} from './modules/website/pages';
 
 const AppRoutes = () => {
   return (
@@ -18,6 +20,31 @@ const AppRoutes = () => {
         <Route path='/shareunits' element={<Shareunits />} />
         <Route path='/topupwallet' element={<Topupwallet />} />
         <Route path='/topupunits' element={<Topupunits />} />
+        <Route path='/admin' element={<AdminLogin />} />
+        <Route path='/admin_reg' element={<AdminSignup />} />
+        <Route path='/admin_dashboard' element={<AdminDashboard />} />
+        <Route path='/admin_order' element={<AdminOrder />} />
+        
+        <Route path='/admin_dashboard_order' element={<AdminDashboardOrder />} />
+        <Route path='/admin_billing_setup' element={<AdminBilling />} />
+
+        <Route path='/user_manager' element={<AdminUser />} />
+        
+        
+        <Route path='/admin_manager' element={<AdminManagement />} />
+        
+        <Route path='/adminmanagementdetails' element={<AdminManagementDetails />} />
+        
+        <Route path='/admin_settings' element={<AdminSettings />} />
+
+        
+
+
+        
+
+
+
+
 
 
 
